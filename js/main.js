d14e6ddc3f536eb072c7211bc670ea84b5434b61
zaynalov005@mail.ru <zaynalov005@mail.ru>
2023-06-15 22:49:32 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -2,4 +2,22 @@ $(document).ready(function(){
     $(".submenu-btn").click(function(){
       $(".submenu").toggleClass("show");
     });
+});
+
+$(document).on("click", function(event){
+  if(!$(event.target).closest(".submenu-btn").length){
+      $(".submenu").removeClass("show");
+  }
+});
+
+$(document).ready(function(){
+  $(".hdr-btn").click(function(){
+    $(".hdrmenu").toggleClass("show");
   });
+});
+
+$(document).on("click", function(event){
+if(!$(event.target).closest(".hdr-btn").length){
+    $(".hdrmenu").removeClass("show");
+}
+});
